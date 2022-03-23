847d7c227c9e5b7ca323ec4b945746803471170c
cipri74 <61431797+cipri74@users.noreply.github.com>
2022-03-23 07:25:35 +0200

    implement the export of all books by category in csv file.

---
+++ library.py
@@ -0,0 +1,94 @@
+import csv
+import requests
+
+
+class LibraryAPI:
+    URL = "https://openlibrary.org"
+
+    def __init__(self, in_file="book_price.csv"):
+        self.__in_file = in_file
+        self.__book = self.URL + "/works"
+        self.__subjects = self.URL + "/subjects"
+        self.__author = self.URL + "/authors"
+        self.__book_prices = self.load_prices()
+
+    def get_url_category_list(self, category, limit=12, offset=0):
+        return self.__subjects + f"/{category}.json?limit={limit}&offset={offset}"
+
+    def export_all_books_by_category(self, category, csv_writer, offset=0, chunk=2, max_rows=2):
+        url = self.get_url_category_list(category, chunk, offset)
+        response = requests.get(url)
+        book_details = self.get_books(response.json())
+        csv_writer.writerows(book_details)
+        nr_rows = len(book_details)
+        while len(response.json().get("works")) > 0 and nr_rows < max_rows:
+            offset += (chunk+1)
+            url = self.get_url_category_list(category, chunk, offset)
+            response = requests.get(url)
+            book_details = self.get_books(response.json())
+            csv_writer.writerows(book_details)
+            nr_rows += len(book_details)
+
+    def get_books(self, json_response):
+        result = []
+        for data in json_response.get("works"):
+            book_id = data.get("key", None)
+            if book_id is None:
+                raise Exception("No book ID found")
+            new_book = self.get_book_detail(book_id)
+            result.append(new_book)
+
+        return result
+
+    def get_book_detail(self, book_id):
+        book_detail = []
+        book_url = f"{self.URL}{book_id}.json"
+        response = requests.get(book_url)
+        raw_id = book_id.split("/")[-1]
+        book_detail.append(raw_id)
+        book_detail.append(response.json().get("title"))
+        book_detail.append(self.get_category(response.json()))
+        book_detail.append(self.get_authors(response.json().get("authors")))
+        book_detail.append(self.__book_prices.get(raw_id, "price"))
+        book_detail.append(self.get_description(response.json()))
+        return book_detail
+
+    def get_authors(self, authors_response):
+        names = []
+        if not isinstance(authors_response, list):
+            return ""
+        for author in authors_response:
+            author_id = (author.get("author")).get("key")
+            author_url = f"{self.URL}{author_id}.json"
+            response = requests.get(author_url)
+            names.append(response.json().get("name"))
+
+        return ";".join(names)
+
+    def load_prices(self):
+        with open(self.__in_file) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=",")
+            result = {}
+            for idx, row in enumerate(csv_reader):
+                if idx > 0:
+                    result[row[0]] = row[1]
+            return result
+
+    @staticmethod
+    def get_description(response):
+        description = response.get("description", None)
+        if description is None:
+            excerpts = response.get("excerpts", None)
+            if excerpts is None:
+                return ""
+            description = ";".join([(excerpt.get("excerpt")).get("value") for excerpt in excerpts])
+        return description
+
+    @staticmethod
+    def get_category(response):
+        subjects = response.get("subjects", None)
+        if subjects:
+            return ";".join([s for s in subjects])
+
+        return ""
+
--- main.py
+++ main.py
@@ -1,8 +1,32 @@
+import csv
+from library import LibraryAPI
+
+
+def upload_google_sheets(csv_file):
+    pass
+
+
+def generate_report(out_file="output.csv"):
+    columns = ["Book_ID", "Book_Title", "Categories", "Authors Names", "Price", "Description"]
+    with open(out_file, "w", newline='') as out:
+        csv_writer = csv.writer(out)
+        csv_writer.writerow(columns)
+        library = LibraryAPI()
+        library.export_all_books_by_category("relational_databases", csv_writer)
+        library.export_all_books_by_category("database_software", csv_writer)
+        library.export_all_books_by_category("python", csv_writer)
+
+    return out_file
 
 
 def main():
-    print("Hello")
+    report_file = generate_report()
+    upload_google_sheets(report_file)
+    # r = requests.get("https://openlibrary.org/works/OL2035966W.json")
+    # print(r.json())
 
 
-if "__name__" == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+main()
